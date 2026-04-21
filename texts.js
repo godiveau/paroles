8229465bd858787f27d1c98b1b972c071ba79748
Gilles GDV <godiveau@users.noreply.github.com>
2026-04-21 15:01:50 +0200

    Correct capitalization in text entries

--- texts.js
+++ texts.js
@@ -2,5 +2,6 @@ window.PAROLES_TEXTS = [
   "Je ne suis pas la mais je peux vous lire.",
   "C'est à proscrire.",
   "Go pour l'actu !",
-  "tarifaire pâté de foie de porc en boite de conserve",
+  "Tarifaire pâté de foie de porc en boite de conserve.",
+  "Si la question n'est pas répondue ici.",
 ];
