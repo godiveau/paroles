37377cb690d24db814749db19205c414d44bc203
Gilles GDV <godiveau@users.noreply.github.com>
2026-04-30 16:33:46 +0200

    Add new phrases to PAROLES_TEXTS array

--- texts.js
+++ texts.js
@@ -1,6 +1,10 @@
 window.PAROLES_TEXTS = [
   "Je ne suis pas la mais je peux vous lire.",
+  "Sombre merde.",
   "C'est à proscrire.",
+  "Conseil des tas.",
+"Faut réenchanter le wording.",
+    "GODI PRUNES du jardin.",
   "Go pour l'actu !",
   "Tarifaire pâté de foie de porc en boite de conserve.",
   "Si la question n'est pas répondue ici.",
